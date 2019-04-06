b13b39944ee963c0b40301b2a38960e9161c3dca
Jaid <jaid.jsx@gmail.com>
2019-04-06 04:07:27 +0200

    Set up basic Photoshop plugin environment
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++
@@ -1,6 +0,0 @@
-import {configureUniversalLib} from "webpack-config-jaid"
-
-export default configureUniversalLib({
-  documentation: true,
-  publishimo: {fetchGithub: true},
-})
---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+module.exports = require("webpack-config-jaid").configurePhotoshopPlugin({
+  title: "Prefew",
+  publishimo: true,
+})
