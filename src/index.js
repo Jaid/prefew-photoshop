24e1ac54b7ad1d5f692c0eb6cc2f614f7cec0ab7
Jaid <jaid.jsx@gmail.com>
2019-04-08 00:51:22 +0200

    Implemented communication with prefew server
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,28 +1,79 @@
 import getLayerByName from "lib/getLayerByName"
 import pixmapToPng from "lib/pixmapToPng"
+import socketIoClient from "socket.io-client"
 
-const main = (generator, config) => {
-  let currentDocumentId
-  let prefewLayerId
-  generator.onPhotoshopEvent("currentDocumentChanged", async documentId => {
-    const document = await generator.getDocumentInfo(documentId, {layerInfo: true})
-    currentDocumentId = documentId
-    prefewLayerId = getLayerByName(document, "prefew")?.id
+const port = 40666
+
+const main = async (generator, config) => {
+  console.log(`Connecting to localhost:${port}`)
+  const socketClient = socketIoClient(`ws://localhost:${port}`, {
+    query: {
+      mode: "provider",
+    },
   })
-  generator.onPhotoshopEvent("documentChanged", async () => {
-    if (!prefewLayerId) {
-      return
+  socketClient.on("connect", async () => {
+    let currentDocument
+    console.log("Connected")
+    const updatePrefewLayer = async documentId => {
+      const document = await generator.getDocumentInfo(documentId, {
+        compInfo: false,
+        imageInfo: false,
+        layerInfo: true,
+        expandSmartObjects: false,
+        getTextStyles: false,
+        getFullTextStyles: false,
+        selectedLayers: false,
+        getCompLayerSettings: false,
+        getDefaultLayerFX: false,
+        getPathData: false,
+      })
+      console.log(`Fetched layer info for document ${document.id}`)
+      const prefewLayer = getLayerByName(document, "prefew")
+      if (prefewLayer) {
+        console.log(`Found layer "prefew" with id ${prefewLayer.id}`)
+        return {
+          id: document.id,
+          prefewLayer: prefewLayer.id,
+        }
+      } else {
+        return {
+          id: document.id,
+        }
+      }
     }
-    const start = Number(new Date)
-    const pixmap = await generator.getPixmap(currentDocumentId, prefewLayerId, {
-      clipToDocumentBounds: true,
-      allowDither: true,
-      maxDimension: 320,
+    const sendImageToPrefew = async () => {
+      const documentId = currentDocument.id
+      const layerId = currentDocument.prefewLayer
+      console.log(`Render pixmap from layer ${layerId} of document ${documentId}`)
+      try {
+        const start = Number(new Date)
+        const pixmap = await generator.getPixmap(documentId, layerId, {
+          clipToDocumentBounds: true,
+          allowDither: true,
+          maxDimension: 320,
+        })
+        const png = pixmapToPng(pixmap)
+        const end = Number(new Date)
+        console.log(`Rendered Pixmap in ${end - start} ms`)
+        socketClient.emit("updateImage", {
+          png,
+          name: "photoshop",
+        })
+      } catch (error) {
+        console.error(error)
+        debugger
+      }
+    }
+    generator.onPhotoshopEvent("currentDocumentChanged", async () => {
+      currentDocument = await updatePrefewLayer()
     })
-    const end = Number(new Date)
-    console.log(`Rendered Pixmap in ${end - start} ms`)
-    const png = pixmapToPng(pixmap)
-    debugger
+    generator.onPhotoshopEvent("documentChanged", () => {
+      console.log("Document changed!")
+      if (currentDocument.prefewLayer) {
+        sendImageToPrefew()
+      }
+    })
+    currentDocument = await updatePrefewLayer()
   })
 }
 
